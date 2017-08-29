752465268a323a1a8c5c83a60d3851bc757a05c7
Clemens Riccabona <clemens@riccabona.it>
2017-08-29 11:52:30 +0200

    [TASK] Make module-loading compatible with v7 and v8 of TYPO3

--- ext_tables.php
+++ ext_tables.php
@@ -1,8 +1,19 @@
 <?php
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+defined('TYPO3_MODE') or die();
 
-if (TYPO3_MODE=='BE')	{
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModulePath('tools_txicsawstatsM1', \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'mod1/');	
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('tools','txicsawstatsM1','',\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'mod1/');
+if (TYPO3_MODE=='BE') {
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+    'tools',
+    'txicsawstatsM1',
+    '',
+    '',
+    [
+      'routeTarget' => \tx_icsawstats_module1::class . '::mainAction',
+        'access' => 'user,group',
+        'name' => 'tools_txicsawstatsM1',
+        'icon' => 'EXT:ics_awstats/mod1/moduleicon.gif',
+        'navigationComponentId' => 'typo3-pagetree',
+        'labels' => 'LLL:EXT:ics_awstats/mod1/locallang_mod.xml'
+    ]
+  );
 }
-?>
